71b5f8a420618d065b7d46312baa4b0b967d23eb
Geneclecio Santos <geneclecio.santos.22@gmail.com>
2023-10-31 21:39:57 -0300

    Aula 01 - 31-10 - transferencia entre telas parte 2

--- app/src/main/java/br/com/etecia/appheroephpmysql/Page2.java
+++ app/src/main/java/br/com/etecia/appheroephpmysql/Page2.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Page2 extends AppCompatActivity {
 
     Button buttonBack;
+    ImageView imgMercado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,17 @@ public class Page2 extends AppCompatActivity {
         setContentView(R.layout.activity_page2);
 
         buttonBack = findViewById(R.id.buttonBack);
+        imgMercado = findViewById(R.id.imgMercado);
+
+        imgMercado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), mercado.class);
+                startActivity(intent);
+
+            }
+        });
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/br/com/etecia/appheroephpmysql/mercado.java
@@ -0,0 +1,34 @@
+package br.com.etecia.appheroephpmysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class mercado extends AppCompatActivity {
+
+    Button buttonBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mercado);
+
+        buttonBack = findViewById(R.id.buttonBack);
+
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), Page2.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
